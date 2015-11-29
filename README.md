f5449647d23b99597b0e5b5003d05b54ea43275f
Kristen Pairitz <ktpairit@gmail.com>
2015-11-29 15:21:23 -0500

    add explanation to README file & new jQuery plugin TURNBOX.js

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # URL
 
 # Resources
-*
+**[jQuery Backstretch](http://srobbin.com/jquery-plugins/backstretch/)
+*This plugin allows you to add a background image to any page or element which will be automatically and dynamically resized.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+//Below is jQuery code for a plugin called Backstretch.  This plugin allows you to add a background image to any page or element which will be automatically and dynamically resized.
+
 $( document ).ready(function() {
-
     $.backstretch("img/Family.Photos.Breck-83b.png");
-
 });
